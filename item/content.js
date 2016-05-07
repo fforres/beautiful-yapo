bce08e629cc473cddc50a183c3333098c802b415
felipe torres <felipe.torressepulveda@gmail.com>
2016-05-07 14:45:08 -0300

    adding the rest of the pages

--- item/content.js
+++ item/content.js
@@ -1,8 +1,17 @@
-/*
 var siteWrapper = document.getElementById("site-wrapper");
+var header = document.getElementById("header");
 var newContent = document.createElement('div');
 newContent.setAttribute("id", "newContent");
 siteWrapper.appendChild(newContent);
+
+var content = document.getElementById("content");
+var adserverRight = document.getElementById("adserver-right");
+
+newContent.appendChild(content.cloneNode(true))
+newContent.appendChild(adserverRight.cloneNode(true))
+
+/*
+var siteWrapper = document.getElementById("site-wrapper");
 
 newContent = document.getElementById('newContent');
 
